b234d7fb7f523101353cc7c0b628729a862dbe4c
Emanuel D R Sena <dariosena@protonmail.com>
2024-04-05 16:39:56 -0300

    plugins(config): add comments and git signs plugins

--- init.lua
+++ init.lua
@@ -24,3 +24,30 @@ vim.opt.cursorline = true
 -- minimal number lines above and bellow cursor
 vim.opt.scrolloff = 10
 
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+
+if not vim.loop.fs_stat(lazypath) then
+  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+  vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+require('lazy').setup({
+  'numToStr/Comment.nvim',
+
+  {
+    'lewis6991/gitsigns.nvim',
+    opts = {
+      signs = {
+        add = { text = '+' },
+	change = { text = '~' },
+	delete = { text = '_' },
+	topdelete = { text = '‾' },
+        changedelete = { text = '~' },
+      },
+    },
+  },
+})
+
+
